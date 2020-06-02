804d5cd97c140546433983615ff527d79139e920
JonZhao <1044264932@qq.com>
2020-06-02 16:36:17 +0800

    minor update

--- image_inpainting.m
+++ image_inpainting.m
@@ -5,11 +5,11 @@ clc;
 % config
 image_path = './test_images/5.jpg';
 patch_size = 9;
-image_data = imread(image_path);
-image_data = double(image_data);
+image_data = im2double(imread(image_path));
 % get missing region from user
 % missing region point out where the missing pixels are in image
-imshow(image_data/255);
+imshow(image_data);
+
 % use mouse to get some coordination by clicking
 [x, y] = ginput;
 % after clicking, you should press ENTER
@@ -17,8 +17,8 @@ imshow(image_data/255);
 target_region = poly2mask(x, y, size(image_data,1), size(image_data, 2));
 image_data = image_data.*(1-target_region);
 % show the masked image
-imshow(image_data/255);
-imwrite(image_data/255, 'masked_image.jpg');
+imshow(image_data);
+imwrite(image_data, 'masked_image.jpg');
 % init
 [image_data, Information] = init(image_data, patch_size, target_region);
 % while there are some missing pixels in image, inpaint the image
@@ -31,11 +31,11 @@ while ~Information.Boundary.is_empty
     % brute force filling
     image_data = brute_force_filling(image_data, coordinate, Information);
     % comment the imshow() will speed up the program
-    imshow(image_data/255);
+    imshow(image_data);
     % update some infomation which help to inpaint image
     Information = update_information(image_data, coordinate, Information);
 end
 toc
 
-imwrite(image_data/255, 'image_inpainted.jpg');
-imshow(image_data/255);
+imwrite(image_data, 'image_inpainted.jpg');
+imshow(image_data);
